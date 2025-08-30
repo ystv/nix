917c1a99a56b4350b3ad3020e9a399b9ea45f9cd
Mia Moir <me@archess.mn>
2025-08-30 17:11:50 +0100

    Remove duplicate nixpkgs declaration

--- nix-builder/default.nix
+++ nix-builder/default.nix
@@ -111,9 +111,6 @@ in
   # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
